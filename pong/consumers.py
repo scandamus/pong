08487b187d4206a42d9fa32628d882456d4a5236
chanma <yunosuke.homma@icloud.com>
2024-05-09 22:05:36 +0900

    AsyncWebSocketConsumerを継承していなかったために非同期処理をうまく行えなかった😡(きづくのめっちゃ時間かかった)

--- pong/consumers.py
+++ pong/consumers.py
@@ -2,7 +2,7 @@ import json
 import asyncio
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from datetime import datetime as dt
 from .game_logic import Paddle, Ball
 
@@ -14,7 +14,8 @@ CANVAS_WIDTH = 600
 CANVAS_HEIGHT = 300
 
 
-class PongConsumer(WebsocketConsumer):
+# 非同期通信を実現したいのでAsyncWebsocketConsumerクラスを継承
+class PongConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
         self.room_name = None
@@ -24,39 +25,40 @@ class PongConsumer(WebsocketConsumer):
         self.ball = Ball(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 10)
         self.is_active = False
 
-    def connect(self):
+    async def connect(self):
         try:
             self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
             self.room_group_name = f"pong_{self.room_name}"
             logger.info(f"Room name: {self.room_name}, Room group name: {self.room_group_name}")
-            async_to_sync(self.channel_layer.group_add)(
+            await self.channel_layer.group_add(
                 self.room_group_name, self.channel_name
             )
 
-            self.accept()
+            await self.accept()
 
             # send updated ball pos until game end
 
         except Exception as e:
             logger.error(f"Error connecting: {e}")
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
         self.is_active = False
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data=None, bytes_data=None):
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        # if message == 'socket_status':
-        #     status = text_data_json['status']
-        #     print(f"socket_status received: {status}")
-        #     # クライアント側でonopenが発火したらループを開始する
-        #     self.schedule_ball_update()
+        if message == 'socket_status':
+            status = text_data_json['status']
+            print(f"socket_status received: {status}")
+            # クライアント側でonopenが発火したらループを開始する
+            self.is_active = True
+            await self.schedule_ball_update()
         if message == 'key_event':
             key = text_data_json['key']
             is_pressed = text_data_json['is_pressed']
@@ -73,7 +75,7 @@ class PongConsumer(WebsocketConsumer):
                     self.paddle2.move("down", CANVAS_HEIGHT)
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(self.room_group_name, {
+        await self.channel_layer.group_send(self.room_group_name, {
             # typeキーはgroup_send メソッド内で指定されるキーで、どのハンドラ関数をトリガするかを指定する
             "type": "pong.message",
             # ここで二つのキーを渡すことでpong_message内で辞書としてアクセスできる
@@ -82,7 +84,7 @@ class PongConsumer(WebsocketConsumer):
         })
 
     # Receive message from room group
-    def pong_message(self, event):
+    async def pong_message(self, event):
         timestamp = event["timestamp"]
         message = event["message"]
         # ball = event["ball"]
@@ -90,7 +92,7 @@ class PongConsumer(WebsocketConsumer):
         # paddle2 = event["paddle2"]
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             # "message": message + f'\n{timestamp}\n\np2={paddle2}\n\nball={ball}\n\np1={paddle1}',
             "message": message,
             "paddle1": {
@@ -118,7 +120,6 @@ class PongConsumer(WebsocketConsumer):
     async def schedule_ball_update(self):
         while self.is_active:
             await asyncio.sleep(0.05)  # 50ミリ秒ごとに待機
-            print('<  schedule_ball_update  >')
             game_status = await self.update_ball_pos()  # ボールの位置を更新し、ゲーム続行かどうかを判定
             if not game_status:
                 break  # ゲームオーバーならループを終了
@@ -132,7 +133,7 @@ class PongConsumer(WebsocketConsumer):
             return True
 
     async def send_game_data(self, game_status):
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             "game_status": game_status,
             "ball": {
                 "x": self.ball.x,
